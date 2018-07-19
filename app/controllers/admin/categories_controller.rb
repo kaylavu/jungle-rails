dab3cc3f8ea14bd33250d42763ceaf4a4de94bab
kaylavu <kaylaavu@gmail.com>
2018-07-19 03:24:46 +0000

    Email sent after user creates an order

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,7 @@
 class Admin::CategoriesController < ApplicationController
+
+    http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"], except: [:index]
+
     def index
         @categories = Category.all
       end
