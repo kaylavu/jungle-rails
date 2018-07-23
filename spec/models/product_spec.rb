18266770dfe3035749cbd1d0466a00d146fd1748
kaylavu <kaylaavu@gmail.com>
2018-07-23 19:36:33 +0000

    added product rspec tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    @category = Category.find_or_create_by! name:"Apparel"
+    @product = @category.products.create! ({
+    name:  'Men\'s Classy shirt',
+    quantity: 10,
+    price: 64.99
+    })
+    expect(@product.id).to be_present 
+  end
+
+  it "is not valid without a name" do
+    @category = Category.find_or_create_by! name:"Apparel"
+    @product = @category.products.create ({
+      name: nil, 
+      quantity: 10,
+      price: 64.99
+    })
+    expect(@product.id).to be_nil
+  end
+ 
+  it "is not valid without a price" do 
+    @category = Category.find_or_create_by! name:"Apparel"
+    @product = @category.products.create ({
+      price:nil, 
+      name:  'Men\'s Classy shirt',
+      quantity: 10,
+    })
+    expect(@product.id).to be_nil
+  end 
+
+  it "is not valid without a quantity" do 
+    @category = Category.find_or_create_by! name:"Apparel"
+    @product = @category.products.create ({
+      quantity: nil, 
+      name:  'Men\'s Classy shirt',
+      price: 64.99
+    })
+    expect(@product.id).to be_nil
+    puts @product.errors.full_messages
+  end 
+
+  it "is not valid without a category" do 
+    @product = Product.new({
+    category: nil, 
+    name:  'Men\'s Classy shirt',
+    quantity: 10,
+    price: 64.99
+    })
+    expect(@product.id).to be_nil
+  end 
+
 end
